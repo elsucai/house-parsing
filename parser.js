db7bb5b7b28fcc1d4e4041cf4303221a1e27515b
Xiang <Xiang@arcticfox>
2015-08-02 17:38:51 -0400

    added command line arg

--- parser.js
+++ parser.js
@@ -1,31 +1,68 @@
+'use strict';
+
 var request = require('request');
-var cheerio = require('cheerio');
 
 var options = {
-	//url: 'http://www.zillow.com/homes/164-Forest-St-Arlington,-MA-02474_rb/',
-	//url: 'https://www.redfin.com/MA/Arlington/164-Forest-St-02474/home/8434134',
-	url: 'http://vow.mlspin.com/idx/details.aspx?mls=71881590&aid=BB985691',
 	method: 'GET',
 	headers: {
-//		'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-//		'Upgrade-Insecure-Requests': 1,
-//		'Host': 'www.redfin.com',
 		'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.125 Safari/537.36'
 	}
 };
 
 var parse = function (body) {
-	var $ = cheerio.load(body);
-	var tmp = $('.Details');
-	console.log('tmp = ' + tmp.html());
-//	console.log($.html());
+	var assembledAddress = /"assembledAddress":"(.*?)",/.exec(body)[1];
+	var city = /"city":"(.*?)",/.exec(body)[1];
+	var state = /"state":"(.*?)",/.exec(body)[1];
+	var zip = /"zip":"(.*?)",/.exec(body)[1];
+	var lotSize = /"lotSize":(.*?),/.exec(body)[1];
+	var price = /"amount":(.*?),/.exec(body)[1];
+	var beds = /"beds":(.*?),/.exec(body)[1];
+	var baths = /"baths":(.*?),/.exec(body)[1];
+	var year = /"yearBuilt":(.*?),/.exec(body)[1];
+	var daysOnRedfin = /"daysOnRedfin":(.*?),/.exec(body)[1];
+	var livingSize = /"sqFt":\{"value":(.*?)\}/.exec(body)[1];
+	var type = /\{"header":"Style","content":(.*?)"\},/.exec(body)[1];
+	var openHouse = /Open House,\s(.*?),\s(.*?),\s([a-z0-9]*?-[a-z0-9]*?)\s/.exec(body);
+	if (openHouse) {
+		openHouse = openHouse[1]+ ',' + openHouse[2] + ',' + openHouse[3];
+	}
+
+	return {
+		address: assembledAddress + ', ' + city + ', ' + zip + ' ' + state,
+		livingSize: livingSize,
+		lotSize: lotSize,
+		beds: beds,
+		baths: baths,
+		type: type,
+		year: year,
+		price: '$' + price,
+		days: daysOnRedfin + ' days',
+		openHouse: openHouse
+	};
 };
 
-request.get(options, function (error, response, body) {
+var sqft2sqm = function (sqftStr) {
+	var n = Number(sqftStr);
+	return Math.ceil(n/10.764);
+};
+
+var main = function (options) {
+	var url = process.argv[2];
+	options.url = url;
+	request.get(options, function (error, response, body) {
 	if (response.statusCode === 200) {
 		var result = parse(body);
+		console.log('address: ' + result.address);
+		console.log('type: ' + result.type + ', built in: ' + result.year);
+		console.log(result.beds + ' bedrooms, ' + result.baths + ' baths');
+		console.log('living Area: ' + result.livingSize + ' sqft (' + sqft2sqm(result.livingSize) + ' SquareMeters)');
+		console.log('lot size: ' + result.lotSize + ' sqft (' + sqft2sqm(result.lotSize) + ' SquareMeters)');
+		console.log('price: ' + result.price + ', ' + result.days + ' on Redfin');
+		console.log('OpenHouse: ' + result.openHouse);
 	} else {
 		console.log('error = ' + error);
 	}
 });
+}
 	
+main(options);
